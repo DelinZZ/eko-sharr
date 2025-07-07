416c1282aa302febec400b29b64ae41b665391dd
Delin <delinzeqiri58@gmail.com>
2025-07-07 15:17:30 +0200

    fixing the code

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,24 +1,27 @@
-  import React from "react";
-  import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-  import Layout from "./Layout";
-  import Home from "./Home";
-  import About from "./About";
-  import Products from "./Products";
-  import Contact from "./Contact";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Home";
+import About from "./About";
+import Products from "./Products";
+import Contact from "./Contact";
 
-  function App() {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="products" element={<Products />} />
-            <Route path="contact" element={<Contact />} />
-          </Route>
-        </Routes>
-      </Router>
-    );
-  }
+// ✅ Import your Tailwind CSS here!
+import "./App.css";
 
-  export default App;
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
